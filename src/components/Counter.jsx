80318075b3c2b4e0ad3f46dd73c4d23abe540d38
Robson Barbosa Souza <powerrob92@gmail.com>
2021-06-02 18:51:25 -0300

    chore: state in React

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,18 @@
-export function Counter(){
+import { useState } from "react";
+
+export function Counter() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(state => state + 1);
+  }
+
   return (
     <div>
-      <h2>0</h2>
-      <button>adicionar</button>
+      <h2>{count}</h2>
+      <button type='button' onClick={increment}>
+        Increment
+      </button>
     </div>
   )
 }
